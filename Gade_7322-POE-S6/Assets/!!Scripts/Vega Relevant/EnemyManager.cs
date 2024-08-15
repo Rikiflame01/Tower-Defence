5bc672ba47255798b32143135dbab63aca978cf7
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-08-15 14:42:31 +0200

    Game Flow Update 2

--- Gade_7322-POE-S6/Assets/!!Scripts/Vega Relevant/EnemyManager.cs
+++ Gade_7322-POE-S6/Assets/!!Scripts/Vega Relevant/EnemyManager.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager instance;
 
     private List<GameObject> enemies = new List<GameObject>();
+    private bool spawningStopped = false;
+    private float spawnCheckInterval = 5f;
 
     private void Awake()
     {
@@ -23,11 +26,13 @@ public class EnemyManager : MonoBehaviour
     private void OnEnable()
     {
         EventManager.instance.onEnemySpawned.AddListener(AddEnemy);
+        EventManager.instance.onWaveMode.AddListener(OnWaveModeStart);
     }
 
     private void OnDisable()
     {
         EventManager.instance.onEnemySpawned.RemoveListener(AddEnemy);
+        EventManager.instance.onWaveMode.RemoveListener(OnWaveModeStart);
     }
 
     private void AddEnemy(GameObject enemy)
@@ -35,6 +40,28 @@ public class EnemyManager : MonoBehaviour
         if (enemy.activeInHierarchy)
         {
             enemies.Add(enemy);
+            spawningStopped = false;
+            StopCoroutine(CheckForWaveEnd());
+        }
+    }
+
+    private void OnWaveModeStart()
+    {
+        StartCoroutine(CheckForWaveEnd());
+    }
+
+    private IEnumerator CheckForWaveEnd()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnCheckInterval);
+
+            if (enemies.Count == 0 && spawningStopped)
+            {
+                EventManager.instance.TriggerCooldownMode();
+                yield break;
+            }
+            spawningStopped = true;
         }
     }
 
